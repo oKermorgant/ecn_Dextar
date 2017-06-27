23c2e63fab87131d77196518c5c31613cf531b34
SaAPro <alexandre_sa@live.fr>
2017-06-27 18:25:04 +0200

    parser up-date/remove fps/resize img/display contours/display name file saved/GUI up-date as fully runable app as it is/configFile up-date (add of the progMode, pixelized and remove live)/add of the pixelized effect/rename the executable file as DrawBOT instead of chains

---
+++ Sarazin-Guda2017/build/CMakeFiles/DrawBOT.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DrawBOT.dir/src/chain.cpp.o"
+  "CMakeFiles/DrawBOT.dir/main/chains.cpp.o"
+  "DrawBOT.pdb"
+  "DrawBOT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DrawBOT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
